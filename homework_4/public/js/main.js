b5d239510f4318bc68a375e96ecea79ef600c15b
Arseniy Gadzhiev <yudaarsen.ca@gmail.com>
2023-05-22 18:44:21 +0300

    4 work

---
+++ homework_4/public/js/main.js
@@ -0,0 +1,52 @@
+/* global location, WebSocket */
+var host = location.origin.replace(/^http/, 'ws')
+var client = new WebSocket(host)
+
+var textbox = document.getElementsByTagName('input')[0]
+var button = document.getElementsByTagName('button')[0]
+
+client.onmessage = function (message) {
+  if (message.data === 'loginSuccess') {
+    button.innerHTML = 'Отправить'
+    textbox.placeholder = 'Текст'
+    textbox.parentElement.className += ' has-success'
+    return
+  } else if (message.data === 'loginFailed') {
+    textbox.parentElement.className += ' has-error'
+    textbox.placeholder = 'Имя пользователя уже используется'
+    return
+  }
+
+  var chatbox = document.getElementById('chatbox')
+  var div = document.createElement('div')
+  div.className = 'col-xs-12 nopadding'
+  div.innerHTML = '<span class="col-xs-11 nopadding">' +
+    message.data.replace(/(\w+)/ig, '<b>$1</b>') +
+    '</span><span class="col-xs-1 text-right nopadding">' +
+    new Date().toTimeString().split(' ')[0] + '</span>'
+
+  chatbox.appendChild(div)
+  chatbox.scrollTop = chatbox.scrollHeight
+}
+
+function send () {
+  if (!textbox.value) return
+
+  var message = {
+    type: 'message',
+    data: textbox.value
+  }
+
+  if (button.innerHTML === 'Войти') {
+    message.type = 'login'
+  }
+
+  client.send(JSON.stringify(message))
+  textbox.value = ''
+}
+
+button.onclick = send
+
+textbox.onkeypress = function (event) {
+  if (event.charCode === 13) send()
+}
---
+++ homework_4/websocket.js
@@ -0,0 +1,50 @@
+var WebSocketServer = require('ws').Server
+
+module.exports = (server) => {
+  var wss = new WebSocketServer({ server: server })
+
+  wss.broadcast = function (data) {
+    wss.clients.forEach(function (client) {
+      client.send(data)
+    })
+  }
+
+  wss.isUsernameAvailable = function (str) {
+    if (!str || typeof str !== 'string') return false
+    for (let client of wss.clients) {
+      if (new RegExp('^' + client.username + '$', 'i').test(str)) return false
+    }
+    return true
+  }
+
+  wss.on('connection', function (socket) {
+    socket.on('message', function (message) {
+      try {
+        message = JSON.parse(message)
+      } catch (e) {
+        console.log(e.message)
+      }
+
+      switch (message.type) {
+        case 'login':
+          if (wss.isUsernameAvailable(message.data)) {
+            socket.username = message.data
+            socket.send('loginSuccess')
+            wss.broadcast(message.data + ' logged in.')
+          } else {
+            socket.send('loginFailed')
+          }
+          break
+        case 'message':
+          wss.broadcast(socket.username + ': ' + message.data)
+          break
+      }
+    })
+
+    socket.on('close', function () {
+      if (socket.username) {
+        wss.broadcast(socket.username + ' logged out.')
+      }
+    })
+  })
+}
